2ccd5367657f41370c6d03bc9d41e345cbe49ad9
AureBac <aureliano.barcellar@gmail.com>
2020-05-05 19:02:08 +0200

    Affichage de la liste de projets preliminaire

--- projet/taskmanager/views.py
+++ projet/taskmanager/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from taskmanager.models import Project
 # Create your views here.
+@login_required()
+def projects(request):
+    #args={'user':request.user}
+    projects=Project.objects.filter(members__username=request.user.username)
+    #return render(request, 'projects.html', args)
+    return render(request, 'projects.html', {'projects':projects})
