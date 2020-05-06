78f78f7c5a085e101fa65c7db39d42a513a3debd
AureBac <aureliano.barcellar@gmail.com>
2020-05-06 04:00:42 +0200

    Page de visualisation des taches preliminaire terminée

--- projet/taskmanager/views.py
+++ projet/taskmanager/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from taskmanager.models import Project
+from taskmanager.models import Project, Task
 # Create your views here.
 @login_required()
 def projects(request):
-    #args={'user':request.user}
     projects=Project.objects.filter(members__username=request.user.username)
-    #return render(request, 'projects.html', args)
     return render(request, 'projects.html', {'projects':projects})
 
 
 @login_required()
 def project(request, id):
-    project=Project.objects.get(id=id)
-    return render(request, 'project.html',{'project':project})
+    projectlist=Project.objects.filter(id=id)
+    tasks=Task.objects.filter(project_id=id)
+    return render(request, 'project.html',{'projectlist':projectlist,'tasks':tasks })
